9ccac4feda905503d37d4372684252d2aeb821b4
Anitsa Kalinova <anitsa011@gmail.com>
2026-07-25 12:03:42 +0200

    01- even sum - f

---
+++ TypeScript/01_Introduction to TypeScript/01_EvenSum.ts
@@ -0,0 +1,8 @@
+function evenSum(num1: number, num2: number, num3: number) {
+    const sum = num1 + num2 + num3;
+    return sum % 2 === 0;
+}
+
+console.log(evenSum(1,2,3));
+console.log(evenSum(2,2,3));
+
---
+++ TypeScript/out/01_Introduction to TypeScript/01_EvenSum.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function evenSum(num1, num2, num3) {
+    const sum = num1 + num2 + num3;
+    return sum % 2 === 0;
+}
+console.log(evenSum(1, 2, 3));
+console.log(evenSum(2, 2, 3));
+//# sourceMappingURL=01_EvenSum.js.map
