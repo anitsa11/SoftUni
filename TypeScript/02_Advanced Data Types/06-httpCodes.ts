e865b71090e6493dc926ea9a2a41c9768778b3e2
Anitsa Kalinova <anitsa011@gmail.com>
2026-07-26 18:49:48 +0300

    Create 06-httpCodes.ts

---
+++ TypeScript/02_Advanced Data Types/06-httpCodes.ts
@@ -0,0 +1,30 @@
+interface TextCodes {
+    code: 200 | 201 | 301;
+    text: string;
+}
+
+interface SliceTextCodes {
+    code: 400 | 404 | 500;
+    text: string;
+    printChars?: number;
+}
+
+function httpCodes(param: TextCodes | SliceTextCodes) {
+    switch(param.code) {
+        case 200:
+        case 201:
+        case 301:
+            console.log(param.text);
+            break;
+        case 400:
+        case 404:
+        case 500:
+            console.log(param.text.slice(0, param.printChars));
+            break;
+    }
+}
+
+httpCodes({ code: 200, text: 'OK'});
+httpCodes({ code: 400, text: 'Bad Request', printChars: 4});
+httpCodes({ code: 404, text: 'Not Found'});
+httpCodes({ code: 404, text: 'Not Found', printChars: 3});
