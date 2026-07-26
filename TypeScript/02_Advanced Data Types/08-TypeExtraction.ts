0f554174706e544d3b61f43d0703fd871a264281
Anitsa Kalinova <anitsa011@gmail.com>
2026-07-26 21:23:55 +0300

    Create 08-TypeExtraction.ts

---
+++ TypeScript/02_Advanced Data Types/08-TypeExtraction.ts
@@ -0,0 +1,40 @@
+let names = {
+    fName: 'John',
+    lName: 'Doe',
+    age: 22, 
+    getPersonInfo() {
+        return
+
+        `${this.fName} ${this.lName}, age ${this.age}`
+    }
+};
+
+
+let location = {
+    city: 'Boston',
+    street: 'Nowhere street',
+    number: 13, postalCode: 51225,
+    getAddressInfo() {
+            return `${this.street} ${this.number}, ${this.city}
+
+${this.postalCode}`
+        }
+};
+
+type Names = typeof names;
+type Location = typeof location;
+
+type Person = Names & Location;
+
+function createCombinedFunction(names: Names, location: Location): (person: Person) => void {
+    return function(person: Person) {
+        console.log(`Hello, ${person.getPersonInfo()} from ${person.getAddressInfo()}`)
+    };
+};
+
+
+let combinedFunction = createCombinedFunction(names, location);
+
+let combinedPerson = Object.assign({}, names, location);
+
+combinedFunction(combinedPerson);
