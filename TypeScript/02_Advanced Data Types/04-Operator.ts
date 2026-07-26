8c1041b0d84d7f8d434e70f07b4e1a765d951d0d
Anitsa Kalinova <anitsa011@gmail.com>
2026-07-26 17:10:08 +0300

    Create 04-Operator.ts

---
+++ TypeScript/02_Advanced Data Types/04-Operator.ts
@@ -0,0 +1,26 @@
+function operator(
+    param: string | number | string[],
+    operation: 'Index' | 'Length' | 'Add',
+    operand: number
+) {
+    switch (operation) {
+        case "Index":
+            if (typeof param !== "number")
+                return param[operand];
+            break;
+        case "Length":
+            if (typeof param !== "number")
+                return param.length % operand;
+            break;
+        case "Add":
+            if (!Array.isArray(param))
+                return Number(param) + operand;
+            break;
+
+    }
+}
+
+console.log(operator(['First', 'Second', 'Third'], 'Index', 1));
+console.log(operator('string', 'Index', 4));
+console.log(operator(['Just', 'Two'], 'Length', 5));
+console.log(operator('7', 'Add', 3));
