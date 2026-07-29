35ac2ba186096536459a0318703cbd900bccc7e1
Anitsa Kalinova <anitsa011@gmail.com>
2026-07-29 19:39:32 +0300

    Create 07-Class-counter.ts

---
+++ TypeScript/03_OOP in TypeScript/07-Class-counter.ts
@@ -0,0 +1,15 @@
+class Counter {
+    private static count: number = 0;
+
+    static increment() {
+        Counter.count++;
+    }
+
+    static getCount() {
+        return Counter.count;
+    }
+}
+
+Counter.increment();
+Counter.increment();
+console.log(Counter.getCount());
