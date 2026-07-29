960c092c26903f8506ea00e84f9e8b0fdb4f6516
Anitsa Kalinova <anitsa011@gmail.com>
2026-07-29 16:39:50 +0300

    second solution to exercise 4- abstraction

---
+++ TypeScript/03_OOP in TypeScript/04-Abstraction.ts
@@ -0,0 +1,35 @@
+// interface Animal {
+//     makeSound(): string;
+// }
+
+// class Dog implements Animal {
+//     makeSound(): string {
+//         return "Woof";
+//     }
+// }
+
+// const dog = new Dog();
+// console.log( dog.makeSound());
+
+abstract class Animal {
+    sound: string;
+
+        constructor (sound: string)
+     {
+        this.sound = sound;
+    }
+    makeSound():void {
+        console.log(this.sound);
+    }
+
+}
+
+class Dog extends Animal {
+    constructor (){
+        super( "Woof");
+    }
+
+
+}
+let dog = new Dog();
+dog.makeSound();
