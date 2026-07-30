1d2e46e85a226949048737410d7e08bce6d46696
Anitsa Kalinova <anitsa011@gmail.com>
2026-07-30 16:47:11 +0300

    Create 06-Choose-type.ts

---
+++ TypeScript/04_Exercise Mastering Generics/06-Choose-type.ts
@@ -0,0 +1,24 @@
+type Choose<T, K extends keyof T> =  {
+    [Key in K]: T[Key];
+};
+
+type test = { 
+   name: string,
+   age: number,
+   test:() => string;
+}
+type extracted = Choose<test, 'name' | 'age'>
+
+// type anotherType = {
+//     time: Date,
+//     duration: number,
+//     test: () => string,
+//     val: 200 | 300,
+//     user: {
+//         name: string,
+//         age: number
+//     }
+// }
+
+// type nestedUserAndTime = 'user' | 'time'
+// type extracted2 = Choose<anotherType, nestedUserAndTime>
