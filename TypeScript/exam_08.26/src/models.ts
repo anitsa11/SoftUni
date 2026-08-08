df344b09ce593625df704476ac31b3ce0bb1a7e7
Anitsa Kalinova <anitsa011@gmail.com>
2026-08-08 09:20:48 +0300

    create files

---
+++ TypeScript/exam_08.26/src/models.ts
@@ -0,0 +1,20 @@
+export enum TowerType {
+    Cannon = "CANNON",
+	Magic  = "MAGIC",
+	Ice    = "ICE",
+}
+
+export interface WithId {
+    id: number;
+}
+
+export interface TowerBase extends WithId {
+	name: string;
+	level: number;
+	type: TowerType;
+}
+
+export interface Enemy extends WithId {
+    name: string;
+	health: number;
+}
